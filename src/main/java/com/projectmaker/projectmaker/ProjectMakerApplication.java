c26c8e3603bfb1b77d5ca65551ed7715a08c51bb
Sandro Haiden <sandrohc2010@hotmail.com>
2019-11-23 03:55:17 -0400

    Inicializando o Projeto
    Arquivos iniciais do projeto.

---
+++ src/main/java/com/projectmaker/projectmaker/ProjectMakerApplication.java
@@ -0,0 +1,13 @@
+package com.projectmaker.projectmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectmaker/projectmaker/ProjectMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectmaker.projectmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
